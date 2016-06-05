58b0ae20d7d2d7c768899477fcd1d3ed99458fef
legendaryleo7 <legendaryleo7@hotmail.com>
2016-06-04 22:01:24 -0400

    Update test_pullup_resistor.py

--- test_pullup_resistor.py
+++ test_pullup_resistor.py
@@ -9,12 +9,12 @@ GPIO.setup("XIO-P1", GPIO.IN)
 
 
 #def readinput():
-  if GPIO.input("XIO-P0"):
-    print(1)
-  else:
-    print(0)
+if GPIO.input("XIO-P0"):
+  print(1)
+else:
+  print(0)
   
-  if GPIO.input("XIO-P1"):
-    print(1)
-  else:
-    print(0)
+if GPIO.input("XIO-P1"):
+  print(1)
+else:
+  print(0)
