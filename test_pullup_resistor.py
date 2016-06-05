2b48da11dd553c354a53a87db59690f00d0f1483
legendaryleo7 <legendaryleo7@hotmail.com>
2016-06-04 21:57:11 -0400

    Update test_pullup_resistor.py

--- test_pullup_resistor.py
+++ test_pullup_resistor.py
@@ -2,13 +2,13 @@ import CHIP_IO.GPIO as GPIO
 
 
 GPIO.setup("XIO-P0", GPIO.IN)
-GPIO.setup("XIO-P0", GPIO.HIGH)
+
 
 GPIO.setup("XIO-P1", GPIO.IN)
-GPIO.setup("XIO-P1", GPIO.HIGH)
 
 
-while 1==1 :
+
+def readinput()
   if GPIO.input("XIO-P0"):
     print(1)
   else:
