61f480cd1c1dddcceedc6280fbe4e49d13a2adae
legendaryleo7 <legendaryleo7@hotmail.com>
2016-06-09 13:50:41 -0400

    Initial commit

---
+++ relay.py
@@ -0,0 +1,14 @@
+#this is an abstraction layer to quickly toggle a GPIO pin between Low and High
+
+
+import Adafruit_GPIO as GPIO
+
+class RelayOutput:
+	def __init__(self, relay_pin):
+		#define the relay pin:
+		self.relayPIN = relay_pin
+		GPIO.BaseGPIO.setup(self, self.relayPIN, OUT,)
+	def relay_on(self):
+		GPIO.BaseGPIO.output(self, self.relayPIN, True)
+	def relay_off(self):
+		GPIO.BaseGPIO.output(self, self.relayPIN, False)
