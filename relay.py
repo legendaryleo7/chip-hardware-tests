b30ab9060246ac01cd9bf600f061eaa00e2ffedf
legendaryleo7 <legendaryleo7@hotmail.com>
2016-06-09 16:51:47 -0400

    Update relay.py

--- relay.py
+++ relay.py
@@ -7,7 +7,7 @@ class RelayOutput:
 	def __init__(self, relay_pin):
 		#define the relay pin:
 		self.relayPIN = relay_pin
-		GPIO.BaseGPIO.setup(self, self.relayPIN, OUT,)
+		GPIO.BaseGPIO.setup(self, self.relayPIN, GPIO.OUT,)
 	def relay_on(self):
 		GPIO.BaseGPIO.output(self, self.relayPIN, True)
 	def relay_off(self):
