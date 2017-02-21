813346f3cba3621c8bf22a6c3852504c3387fec0
Leonel Morales <legendaryleo7@hotmail.com>
2017-02-21 13:29:25 -0500

    add rc tests

---
+++ rctestdelta.py
@@ -0,0 +1,17 @@
+import rotary_encoder
+
+A_PIN = "XIO-P0"
+B_PIN = "XIO-P1"
+
+encoder = rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
+encoder.start()
+
+
+while 1:
+    delta = encoder.get_delta()
+    if delta != 0:
+        print "rotated %d" % delta
+
+except KeyboardInterrupt:
+    import CHIP_IO.Utilities as UT
+    UT.unexport_all()
