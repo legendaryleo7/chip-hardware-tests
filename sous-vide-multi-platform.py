6f27105f3f1f2c901e514c3aa7a105875d22389f
Leonel Morales <legendaryleo7@hotmail.com>
2017-07-01 13:20:41 -0400

    Updates file to follow some naming standards. Still needs to be tested on a chip

--- sous-vide-multi-platform.py
+++ sous-vide-multi-platform.py
@@ -1,178 +1,191 @@
 from w1thermsensor import W1ThermSensor
+from enum import Enum, auto
 import PID
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.PWM as PWM
 import Adafruit_CharLCD as LCD
 import rotary_encoder
 
-# Configure Defaults
-# Configure Units
-celsius_unit = False  # Default to Farnheight
 
-# starting temp
-if celsius_unit is False:
-    target_temp = 130
-elif celsius_unit is True:
-    target_temp = 55
-
-plat = GPIO.Platform.platform_detect()
-
-if plat is 5:
-    # C.H.I.P. CharLCD Configuration
-    lcd_rs = 'LCD-D4'
-    lcd_en = 'LCD-D3'
-    lcd_d4 = 'LCD-D6'
-    lcd_d5 = 'LCD-D5'
-    lcd_d6 = 'LCD-D10'
-    lcd_d7 = 'LCD-D7'
-    lcd_red = 'LCD-D12'
-    lcd_green = 'LCD-D11'
-    lcd_blue = 'LCD-D14'
-
-    # Temperature Sensor pin is LCD-D2
-    temp_pin = "LCD-D2"
-
-    # Relay Pin Assignment
-    relay_pin = "LCD-D13"
-
-    # Rotary Encoder pins
-    rc1_a_pin = "XIO-P0"
-    rc1_b_pin = "XIO-P1"
-
-elif plat is 1:
-    # Raspberry Pi configuration:
-    lcd_rs = 27  # Change this to pin 21 on older revision Raspberry Pi's
-    lcd_en = 22
-    lcd_d4 = 25
-    lcd_d5 = 24
-    lcd_d6 = 23
-    lcd_d7 = 18
-    lcd_red = 4
-    lcd_green = 17
-    lcd_blue = 7  # Pin 7 is CE1
-    # Temperature Sensor pin is LCD-D2
-    temp_pin = 0  # Change this to something useful
-
-    # Relay Pin Assignment
-    relay_pin = 1  # Change this to something useful
-
-elif plat is 2:
-
-    # BeagleBone Black configuration:
-    lcd_rs = 'P8_8'
-    lcd_en = 'P8_10'
-    lcd_d4 = 'P8_18'
-    lcd_d5 = 'P8_16'
-    lcd_d6 = 'P8_14'
-    lcd_d7 = 'P8_12'
-    lcd_red = 'P8_7'
-    lcd_green = 'P8_9'
-    lcd_blue = 'P8_11'
-    # Temperature Sensor pin is LCD-D2
-    temp_pin = 0  # Change this to something useful
-
-    # Relay Pin Assignment
-    relay_pin = 1  # Change this to something useful
+class ThermalUnits(Enum):
+    CELSIUS = auto()
+    FAHRENHEIT = auto()
 
 
-# Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows = 2
+class SousVide:
+    def __init__(self, temp_unit=ThermalUnits.FAHRENHEIT):
 
-# Alternatively specify a 20x4 LCD.
-# lcd_columns = 20
-# lcd_rows    = 4
+        # Initialize Python GPIO Library
+        self.plat = GPIO.Platform.platform_detect()
+        self.gpio = GPIO.get_platform_gpio()
 
-# Initialize GPIO
-gpio = GPIO.get_platform_gpio()
-# Initialize One Wire Thermal Sensor
-sensor = W1ThermSensor()
+        # Set pin assignments
+        self.set_pin_assignments()
 
-# Initialize the LCD using the pins
-lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                              lcd_columns,
-                              lcd_rows, lcd_red, lcd_green, lcd_blue,
-                              gpio=GPIO.get_platform_gpio(),
-                              invert_polarity=True,
-                              enable_pwm=True,
-                              pwm=PWM.get_platform_pwm(pwmtype="softpwm"),
-                              initial_color=(1.0, 1.0, 1.0))
+        # Start the PID
+        self.pid = PID.PID()
+        self.temp_unit = temp_unit
+        self.pid.SetPoint = self.set_default_temperature(self.temp_unit)
 
+        self.lcd_columns = 16
+        self.lcd_rows = 2
+        
+        # Initialize the LCD using the pins
+        self.lcd = LCD.Adafruit_RGBCharLCD(self.lcd_rs,
+                                           self.lcd_en,
+                                           self.lcd_d4,
+                                           self.lcd_d5,
+                                           self.lcd_d6,
+                                           self.lcd_d7,
+                                           self.lcd_columns,
+                                           self.lcd_rows,
+                                           self.lcd_red,
+                                           self.lcd_green,
+                                           self.lcd_blue,
+                                           gpio=GPIO.get_platform_gpio(),
+                                           invert_polarity=True,
+                                           enable_pwm=True,
+                                           pwm=PWM.get_platform_pwm(pwmtype="softpwm"),
+                                           initial_color=(1.0, 1.0, 1.0))
 
-# Blue startup screen
-lcd.set_color(0.0, 0.0, 1.0)
-lcd.clear()
+        # Blue startup screen
+        self.lcd.set_color(0.0, 0.0, 1.0)
+        self.lcd.clear()
 
-# Create Farenheight Degree Symbol
-lcd.create_char(1, [0b11000,
-                    0b11000,
-                    0b00000,
-                    0b00111,
-                    0b00100,
-                    0b00110,
-                    0b00100,
-                    0b00000])
+        # Create Fahrenheit Degree Symbol
+        self.lcd.create_char(1, [0b11000,
+                                 0b11000,
+                                 0b00000,
+                                 0b00111,
+                                 0b00100,
+                                 0b00110,
+                                 0b00100,
+                                 0b00000])
 
+        # Create Celsius Degree Symbol
+        self.lcd.create_char(2, [0b11000,
+                                 0b11000,
+                                 0b00011,
+                                 0b00100,
+                                 0b00100,
+                                 0b00100,
+                                 0b00011,
+                                 0b00000])
 
-# Create Celsius  Degree Symbol
-lcd.create_char(2, [0b11000,
-                    0b11000,
-                    0b00011,
-                    0b00100,
-                    0b00100,
-                    0b00100,
-                    0b00011,
-                    0b00000])
+        # Initialize One Wire Thermal Sensor
+        self.sensor = W1ThermSensor()
 
-# Define a way to poll the sensor
-def update_temp(celsius):
+        # Setup relay for Active Low
+        self.gpio.setup(self.relay_pin, GPIO.OUT)
+        self.gpio.output(self.relay_pin, GPIO.HIGH)
 
-    if celsius is False:
-        return sensor.get_temperature(W1ThermSensor.DEGREES_F)
-    elif celsius is True:
-        return sensor.get_temperature(W1ThermSensor.DEGREES_C)
+        self.pid.setSampleTime(1)
 
+        # Start the rotary encoder
+        self.rotary_encoder0 = rotary_encoder.RotaryEncoder.Worker(self.rc1_a_pin, self.rc1_b_pin)
+        self.rotary_encoder0.start()
 
-gpio.setup(relay_pin, GPIO.OUT)
-# We're using Active Low
-gpio.output(relay_pin, GPIO.HIGH)
+    def get_temperature(self, temp_unit):
+        """
+        Polls the temperature sensor and returns a value based on the current
+        unit measurement.
 
-# Start the PID
-pid = PID.PID()
-pid.SetPoint = target_temp
+        Args:
+            temp_unit: this is an enumeration of the ThermalUnits class
 
-pid.setSampleTime(1)
+        Returns:
+            The current temperature
+        """
+        if temp_unit is ThermalUnits.FAHRENHEIT:
+            return self.sensor.get_temperature(W1ThermSensor.DEGREES_F)
 
-# Start the rotary encoder
-encoder1 = rotary_encoder.RotaryEncoder.Worker(rc1_a_pin, rc1_b_pin)
-encoder1.start()
+        elif temp_unit is ThermalUnits.CELSIUS:
+            return self.sensor.get_temperature(W1ThermSensor.DEGREES_C)
 
-try:
-    while 1:
-        encoder1_delta = encoder1.get_delta()
-        if encoder1_delta != 0:
-            pid.SetPoint = (encoder1_delta * .25) + pid.SetPoint
+    def set_default_temperature(self):
+        if self.temp_unit is ThermalUnits.FAHRENHEIT:
+            return 130
+        elif self.temp_unit is ThermalUnits.CELSIUS:
+            return 55
 
-        temp = update_temp(celsius_unit)
-        pid.update(temp)
-        output = pid.output
-        if output >= 0:
-            gpio.output(relay_pin, GPIO.HIGH)
-            lcd.set_color(1.0, 0.0, 0.0)  # Red
-        else:
-            gpio.output(relay_pin, GPIO.LOW)
-            lcd.set_color(0.0, 0.0, 1.0)  # Blue
+    def set_pin_assignments(self):
+        # Set Pin Assignments for the CHIP
+        if self.plat is 5:
+            # C.H.I.P. CharLCD Configuration
+            self.lcd_rs = 'LCD-D4'
+            self.lcd_en = 'LCD-D3'
+            self.lcd_d4 = 'LCD-D6'
+            self.lcd_d5 = 'LCD-D5'
+            self.lcd_d6 = 'LCD-D10'
+            self.lcd_d7 = 'LCD-D7'
+            self.lcd_red = 'LCD-D12'
+            self.lcd_green = 'LCD-D11'
+            self.lcd_blue = 'LCD-D14'
+            self.temp_pin = "LCD-D2"
+            self.relay_pin = "LCD-D13"
+            # Rotary Encoder pins
+            self.rc1_a_pin = "XIO-P0"
+            self.rc1_b_pin = "XIO-P1"
 
-        lcd.clear()
-        lcd.message('TEMP:{0:0.1f}\x01  \nGOAL:{1:0.1f}\x01'.format(temp, pid.SetPoint))
+        elif self.plat is 1:
+            # Raspberry Pi configuration:
+            self.lcd_rs = 27  # Change this to pin 21 on older revision Raspberry Pi's
+            self.lcd_en = 22
+            self.lcd_d4 = 25
+            self.lcd_d5 = 24
+            self.lcd_d6 = 23
+            self.lcd_d7 = 18
+            self.lcd_red = 4
+            self.lcd_green = 17
+            self.lcd_blue = 7  # Pin 7 is CE1
+            # Temperature Sensor pin is LCD-D2
+            self.temp_pin = 0  # Change this to something useful
 
-        # Provide some feedback at the terminal level
-        # print("PID OUTOUT IS: " + str(output) +
-        #       "and the temperature is: " + str(round(temp, 1)))
-except KeyboardInterrupt:
-    gpio.output(relay_pin, GPIO.HIGH)
-    lcd.clear()
-    if plat is 5:
-        import CHIP_IO.Utilities as UT
-        UT.unexport_all()
-    pass
+            # Relay Pin Assignment
+            self.relay_pin = 1  # Change this to something useful
+
+        elif self.plat is 2:
+
+            # BeagleBone Black configuration:
+            self.lcd_rs = 'P8_8'
+            self.lcd_en = 'P8_10'
+            self.lcd_d4 = 'P8_18'
+            self.lcd_d5 = 'P8_16'
+            self.lcd_d6 = 'P8_14'
+            self.lcd_d7 = 'P8_12'
+            self.lcd_red = 'P8_7'
+            self.lcd_green = 'P8_9'
+            self.lcd_blue = 'P8_11'
+            # Temperature Sensor pin is LCD-D2
+            self.temp_pin = 0  # Change this to something useful
+
+            # Relay Pin Assignment
+            self.relay_pin = 1  # Change this to something useful
+
+    def main(self):
+        try:
+            while 1:
+                encoder1_delta = self.rotary_encoder0.get_delta()
+                if encoder1_delta != 0:
+                    self.pid.SetPoint = (encoder1_delta * .25) + self.pid.SetPoint
+
+                temp = self.get_temperature(self.tempUnit)
+                self.pid.update(temp)
+
+                if self.pid.output >= 0:
+                    self.gpio.output(self.relay_pin, GPIO.HIGH)
+                    self.lcd.set_color(1.0, 0.0, 0.0)  # Red
+                else:
+                    self.gpio.output(self.relay_pin, GPIO.LOW)
+                    self.lcd.set_color(0.0, 0.0, 1.0)  # Blue
+
+                self.lcd.clear()
+                self.lcd.message('TEMP:{0:0.1f}\x01  \nGOAL:{1:0.1f}\x01'.format(temp, self.pid.SetPoint))
+
+        except KeyboardInterrupt:
+            self.gpio.output(self.relay_pin, GPIO.HIGH)
+            self.lcd.clear()
+            if self.plat is 5:
+                import CHIP_IO.Utilities as UT
+                UT.unexport_all()
+            pass
